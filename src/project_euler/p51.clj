bb2711ac63d926acc3db8052ce12ff3f4a1a6fff
Ivory Runaway <dkozhukhar@gmail.com>
2013-06-01 19:12:05 +0300

    p51

--- src/project_euler/p51.clj
+++ src/project_euler/p51.clj
@@ -1,5 +1,6 @@
 (ns project-euler.p51
-   (:require [project-euler.primes  :as primes]))
+   (:require [project-euler.primes  :as primes])
+     (:require [clojure.math.combinatorics :as combo]))
 
 
 
@@ -11,7 +12,7 @@
 
 ;Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
 
-(time 
+(time
 (println
    (for [a (range 10)
          b (range 10)
@@ -23,9 +24,9 @@
          ;:when (odd? c)
          ;:when (pos? (mod (+ a b c ) 3))
          :let [pgen (Integer. (str a b c ))
-               p?gen (primes/prime? pgen)]       
+               p?gen (primes/prime? pgen)]
          :when p?gen
-         :let 
+         :let
            [gens
             (for [t (range 0 10)
                   :let [snum (apply str [ t t a t b c ])]
@@ -41,3 +42,63 @@
       [mgen  lgens pgen p?gen])))
 
 
+
+(defn digitscount [n]
+  (->> n
+       str
+       frequencies
+       vals
+       (apply max)
+    ))
+
+
+(defn analizator [n]
+   [n (frequencies (str n))]
+       )
+
+(println
+ (combo/combinations  [0 1 2 3 4 5] 4))
+
+
+
+
+
+(defn solver [vd va]
+  (->> (primes/primes3 1000000)
+       (filter #(= (+ (count vd) (count va)) (count (str %))))
+       (filter #(<= (count va) (digitscount %)))
+       (group-by
+        (fn [n]
+          [ (apply =  (map #(nth (str n) %) va ))
+           ;(nth (str n) 0)
+           ;(nth (str n) 1)
+           ;(nth (str n) 2)
+            (map #(nth (str n) %) vd)
+
+
+           ]))
+       (filter (fn [[[a & args] coll]] a ))
+       (filter (fn [[cod coll]] (>= (count coll) 8)))
+       ;(sort-by (fn [[cod coll]] (count coll)))
+       ;(map grouper)
+
+
+       ;(map analizator)
+    ))
+
+
+
+
+(time
+ (println
+  (let [inds [0 1 2 3 4 5]
+        diffes  (map set (combo/combinations  inds 3))
+        alls  (map
+                (fn [df]
+                  [df  (clojure.set/difference (set inds) df)]) diffes)]
+
+  (for [[difind samind] alls  ]
+    (solver (vec difind) (vec samind))
+
+    ))))
+;[[true (2 3 3)] [424343 121313 626363 929393 323333 525353 828383 222323]]
