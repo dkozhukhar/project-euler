db1bb7c2ebace535a1b7dc34ad94cddef501d922
Ivory Runaway <dkozhukhar@gmail.com>
2013-06-16 19:58:01 +0300

    pr067

---
+++ src/project_euler/pr067.clj
@@ -0,0 +1,31 @@
+(ns project-euler.pr067)
+
+
+
+(def triangle
+(->>  "http://projecteuler.net/project/triangle.txt"
+      slurp
+      (clojure.string/split-lines)
+      (map #(clojure.string/split % #"[ ]"))
+      (map (fn [coll] (map #(Integer. %) coll)))
+      )
+  )
+
+
+
+(time (println
+(loop [accum-route (first triangle) tmp-route (second triangle) new-routes (drop 2 triangle)]
+  (let [new-accum-route
+          (->>
+               (->>(concat [(first accum-route)] accum-route [(last accum-route)])
+                   (partition 2 1 )
+                   (map #(apply max %))
+                 )
+                 (map + tmp-route)
+              )]
+    (if (empty? new-routes)
+      (apply max new-accum-route)
+      (recur new-accum-route (first new-routes) (rest new-routes)  ))
+     )
+  )))
+
