4156d75fc019066a12fd91156ad9ac89b40992f2
Ivory Runaway <dkozhukhar@gmail.com>
2013-06-18 19:58:25 +0300

    pr070

---
+++ src/project_euler/pr070.clj
@@ -0,0 +1,37 @@
+(ns project-euler.pr070
+    (:require
+    [project-euler.primes  :as primes]))
+
+
+(def pt (take 800 (primes/lazy-primes3)))
+
+(last pt)
+
+(defn perm? [a b] (= (sort (str a)) (sort (str b)) ) )
+
+(defn phi [m1 m2]
+  (if (not= m1 m2)
+    (- (dec (* m1 m2))
+       (dec m2)
+       (dec m1))
+    (- (dec (* m1 m2))
+       (dec m1))))
+
+(def solve070
+(->>
+  (for [t1 pt
+        t2 pt
+        :let [n (* t1 t2)
+              ph (phi t1 t2)
+              ra (/ n ph)]
+        :when (> 10000000 n)
+        :when (perm? n ph  )
+        ]
+    [t1 t2 n ph ra])
+ (sort-by (fn [[x y a b c]] c))
+ first
+ ))
+
+(->> solve070
+     println
+     time)
